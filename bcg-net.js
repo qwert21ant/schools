06ecac8c1edec90dbf2f5aad646830fe9d50ea3a
qwert21 <qwert21qwerty@yandex.by>
2021-02-01 16:47:55 +0300

    Update bcg-net.js

--- bcg-net.js
+++ bcg-net.js
@@ -1,16 +1,5 @@
 var NUM_PARTIC = 300;
 
-/////////
-function rand(min, max){
-	return Math.random() * (max - min) + min;
-}
-
-function dist(l, r){
-	let d1 = l.x - r.x;
-	let d2 = l.y - r.y;
-	return Math.sqrt(d1*d1 + d2*d2);
-}
-
 class Particle{
 	pos   = {x: 0.0, y: 0.0};
 	speed = {x: 0.0, y: 0.0};
@@ -29,36 +18,43 @@ class Particle{
 		this.speed.y = rand(3, 10);
 	}
 	
-	move(){
+	move(bgSize){
 		this.pos.x += this.speed.x;
 		this.pos.y += this.speed.y;
 		
-		if(this.pos.x < 0 || this.pos.x > bcgSize.x || this.pos.y < 0 || this.pos.y > bcgSize.y)
-			init();
+		if(this.pos.x < 0 || this.pos.x > bgSize.x || this.pos.y < 0 || this.pos.y > bgSize.y) init();
 	}
 };
-/////////
 
 function startBG() {
+	var bcgElem = document.getElementsByClassName("bg_type0")[0];
 
-var bcgElem = document.getElementsByClassName("bg_type0")[0];
+	var bcgSize = {x: bcgElem.clientWidth, y: bcgElem.clientHeight};
 
-var bcgSize = {x: bcgElem.clientWidth, y: bcgElem.clientHeight};
+	var cvsElem = document.createElement("canvas");
+	cvsElem.setAttribute("style", "position: absolute; top: 0; left: 0;");
+	cvsElem.setAttribute("width", bcgElem.x);
+	cvsElem.setAttribute("height", bcgElem.y);
 
-var cvsElem = document.createElement("canvas");
-cvsElem.setAttribute("style", "position: absolute; top: 0; left: 0;");
-cvsElem.setAttribute("width", bcgElem.x);
-cvsElem.setAttribute("height", bcgElem.y);
+	var context = cvsElem.getContext("2d");
+	context.fillStyle = "red";
 
-var context = cvsElem.getContext("2d");
-context.fillStyle = "red";
+	var partics = [];
+	for(let i = 0; i < NUM_PARTIC; i++) partics.push(new Particle());
 
-var partics = [];
-for(let i = 0; i < NUM_PARTIC; i++) partics.push(new Particle());
-
-setInterval(() => {
-	for(let i = 0; i < NUM_PARTIC; i++) partics[i].move();
-	
-	console.log('tick');
-}, 10);
+	setInterval(() => {
+		for(let i = 0; i < NUM_PARTIC; i++) partics[i].move(bcgSize);
+		
+		console.log('tick');
+	}, 10);
 };
+
+function rand(min, max){
+	return Math.random() * (max - min) + min;
+}
+
+function dist(l, r){
+	let d1 = l.x - r.x;
+	let d2 = l.y - r.y;
+	return Math.sqrt(d1*d1 + d2*d2);
+}
