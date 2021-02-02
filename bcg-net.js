76a9bf486eaf605f4ab40b16b338b082a9732f41
qwert21 <qwert21qwerty@yandex.by>
2021-02-02 15:50:12 +0300

    Update bcg-net.js

--- bcg-net.js
+++ bcg-net.js
@@ -1,21 +1,23 @@
 var NUM_PARTIC = 300;
+var min_speed = 3;
+var max_speed = 10;
 
 class Particle{
 	pos   = {x: 0.0, y: 0.0};
 	speed = {x: 0.0, y: 0.0};
 	
 	constructor(){
-		this.pos.x = rand(3, 10);
-		this.pos.y = rand(3, 10);
-		this.speed.x = rand(3, 10);
-		this.speed.y = rand(3, 10);
+		this.pos.x = rand(min_speed, max_speed);
+		this.pos.y = rand(min_speed, max_speed);
+		this.speed.x = rand(min_speed, max_speed);
+		this.speed.y = rand(min_speed, max_speed);
 	}
 	
 	init(){
-		this.pos.x = rand(3, 10);
-		this.pos.y = rand(3, 10);
-		this.speed.x = rand(3, 10);
-		this.speed.y = rand(3, 10);
+		this.pos.x = rand(min_speed, max_speed);
+		this.pos.y = rand(min_speed, max_speed);
+		this.speed.x = rand(min_speed, max_speed);
+		this.speed.y = rand(min_speed, max_speed);
 	}
 	
 	move(bgSize){
@@ -34,21 +36,32 @@ function startBG() {
 	console.log(bcgSize);
 
 	var cvsElem = document.createElement("canvas");
-	cvsElem.setAttribute("style", "position: absolute; height: " + bcgSize.y + "px; width: " + bcgSize.x + "px;");
+	cvsElem.setAttribute("style", "position: absolute; width: 100%; height: 100%;");
 	
 	bcgElem.insertBefore(cvsElem, bcgElem.firstChild);
 
 	var context = cvsElem.getContext("2d");
-	context.rect(0, 0, bcgSize.x, bcgSize.y);
-	context.fillStyle = "red";
-	context.fill();
-	context.stroke();
+
+	var gradient = context.createLinearGradient(0, 0, bcgSize.x, bcgSize.y);
+	
+	gradient.addColorStop(0, "gray");
+	gradient.addColorStop(1, "yellow");
 
 	var partics = [];
 	for(let i = 0; i < NUM_PARTIC; i++) partics.push(new Particle());
 
 	setInterval(() => {
 		for(let i = 0; i < NUM_PARTIC; i++) partics[i].move(bcgSize);
+		
+		context.fillStyle = gradient;
+		context.fillRect(0, 0, bcgSize.x, bcgSize.y);
+		
+		for(let i = 0; i < NUM_PARTIC; i++){
+			for(let j = 0; j < NUM_PARTIC; j++){
+				if(i == j) continue;
+				
+			}
+		}
 		
 		console.log('tick');
 	}, 10);
