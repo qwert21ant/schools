9e29388e9205f394f92100b7babcccbeb05c772f
qwert21 <qwert21qwerty@yandex.by>
2021-02-03 00:14:28 +0300

    Update bcg-net.js

--- bcg-net.js
+++ bcg-net.js
@@ -42,7 +42,7 @@ function startBG() {
 
 	var context = cvsElem.getContext("2d");
 
-	var gradient = context.createLinearGradient(0, 0, 10, 10);
+	var gradient = context.createLinearGradient(0, 0, bcgSize.x, bcgSize.y);
 	
 	gradient.addColorStop(0, "gray");
 	gradient.addColorStop(0.3, "yellow");
