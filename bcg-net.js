aa1bfceffcfce83edfe5a5b2ed0f1c83e13a2ec4
qwert21 <qwert21qwerty@yandex.by>
2021-02-03 17:55:28 +0300

    Update bcg-net.js

--- bcg-net.js
+++ bcg-net.js
@@ -38,8 +38,8 @@ function startBG() {
 
 	var cvsElem = document.createElement("canvas");
 	cvsElem.setAttribute("style", "position: absolute; left: 0; top: 0; width: 100%; height: 100%;");
-	cvsElem.setAttribute("width", bcgElem.x.toString());
-	cvsElem.setAttribute("height", bcgElem.y.toString());
+	cvsElem.setAttribute("width", bcgSize.x.toString());
+	cvsElem.setAttribute("height", bcgSize.y.toString());
 	
 	bcgElem.insertBefore(cvsElem, bcgElem.firstChild);
 
