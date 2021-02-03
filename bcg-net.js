e9d13d17e4c5382d7532e8019fed1e04f8637e43
qwert21 <qwert21qwerty@yandex.by>
2021-02-03 22:28:59 +0300

    Rename & end bcg-net.js

--- bcg-net.js
+++ bcg-net.js
@@ -1,7 +1,9 @@
 var NUM_PARTIC = 300;
-var RANGE = 100;
-var min_speed = 3;
-var max_speed = 10;
+var RANGE = 120;
+var PADDING = RANGE / 5;
+var FPS = 100;
+var min_speed = 0.01;
+var max_speed = 1.00;
 
 class Particle{
 	pos   = {x: 0.0, y: 0.0};
@@ -21,11 +23,11 @@ class Particle{
 		this.speed.y = rand(min_speed, max_speed);
 	}
 	
-	move(sz){
-		this.pos.x += this.speed.x;
-		this.pos.y += this.speed.y;
-		
-		if(this.pos.x < 0 || this.pos.x > sz.x || this.pos.y < 0 || this.pos.y > sz.y) this.init(sz);
+	move(sz, c){
+		this.pos.x += this.speed.x * func(c);
+		this.pos.y += this.speed.y * func(c);
+
+		if(this.pos.x < -PADDING || this.pos.x > sz.x + PADDING || this.pos.y < -PADDING || this.pos.y > sz.y + PADDING) this.init(sz);
 	}
 };
 
@@ -33,11 +35,9 @@ function startBG() {
 	var bcgElem = document.getElementsByClassName("main_grid_center_column")[0];
 
 	var bcgSize = {x: bcgElem.clientWidth, y: bcgElem.clientHeight};
-	console.log('Canvas size: ');
-	console.log(bcgSize);
 
 	var cvsElem = document.createElement("canvas");
-	cvsElem.setAttribute("style", "position: absolute; left: 0; top: 0; width: 100%; height: 100%;");
+	cvsElem.setAttribute("style", "position: absolute; left: 0; top: 0;");
 	cvsElem.setAttribute("width", bcgSize.x.toString());
 	cvsElem.setAttribute("height", bcgSize.y.toString());
 	
@@ -45,17 +45,23 @@ function startBG() {
 
 	var context = cvsElem.getContext("2d");
 
-	var gradient = context.createLinearGradient(0, 0, bcgSize.x, bcgSize.y);
-	
+	var gradient = context.createLinearGradient(0, 0, bcgSize.x, bcgSize.y * 0.8);
+	/*
 	gradient.addColorStop(0, "gray");
 	gradient.addColorStop(0.3, "yellow");
 	gradient.addColorStop(1, "red");
-
+	*/
+	
+	gradient.addColorStop(0, "#6BD7FF");
+	gradient.addColorStop(0.6, "#93FFA2");
+	gradient.addColorStop(1, "#8DFF42");
+	
 	var partics = [];
 	for(let i = 0; i < NUM_PARTIC; i++) partics.push(new Particle(bcgSize));
 
+	var c = 0;
 	setInterval(() => {
-		for(let i = 0; i < NUM_PARTIC; i++) partics[i].move(bcgSize);
+		for(let i = 0; i < NUM_PARTIC; i++) partics[i].move(bcgSize, c);
 		
 		context.fillStyle = gradient;
 		context.fillRect(0, 0, bcgSize.x, bcgSize.y);
@@ -64,16 +70,20 @@ function startBG() {
 			for(let j = 0; j < NUM_PARTIC; j++){
 				if(i == j) continue;
 				let d = dist(partics[i].pos, partics[j].pos);
-				if(d < RANGE) line(context, partics[i].pos, partics[j].pos, (100 - d) * 2.55);
+				if(d < RANGE) line(context, partics[i].pos, partics[j].pos, (RANGE - d) / RANGE);
 			}
 		}
-		
-		console.log('tick');
-	}, 100);
+		c++;
+	}, 1000 / FPS);
 };
 
+function func(c){
+	return 1;
+}
+
 function line(ctx, pos1, pos2, alpha){
-	ctx.strokeStyle = 'rgba(255, 255, 255, ' + alpha + ')';
+	//ctx.strokeStyle = 'rgba(255, 255, 255, ' + alpha + ')';
+	ctx.strokeStyle = 'rgba(0, 0, 0, ' + alpha + ')';
 	ctx.beginPath();
 	ctx.moveTo(pos1.x, pos1.y);
 	ctx.lineTo(pos2.x, pos2.y);
@@ -81,7 +91,9 @@ function line(ctx, pos1, pos2, alpha){
 }
 
 function rand(min, max){
-	return Math.random() * (max - min) + min;
+	let res = Math.random() * (max - min) + min;
+	if(Math.random() > 0.5) res *= -1;
+	return res;
 }
 
 function dist(l, r){
